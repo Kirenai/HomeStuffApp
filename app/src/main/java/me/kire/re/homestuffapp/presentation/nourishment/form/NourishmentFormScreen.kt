5aaf8c5f1861dbaed3d33db7a7d2bd4df5574590
Kirenai <pool_dota2@hotmail.com>
2025-05-22 00:34:45 -0500

    feat(nourishment): add image selection and display functionality in NourishmentFormScreen

--- app/src/main/java/me/kire/re/homestuffapp/presentation/nourishment/form/NourishmentFormScreen.kt
+++ app/src/main/java/me/kire/re/homestuffapp/presentation/nourishment/form/NourishmentFormScreen.kt
@@ -1,15 +1,24 @@
 package me.kire.re.homestuffapp.presentation.nourishment.form
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,13 +33,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import me.kire.re.homestuffapp.R
+import me.kire.re.homestuffapp.presentation.common.DisplayImage
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -38,7 +50,6 @@ fun NourishmentFormScreen(
     navigateUp: () -> Unit,
     onSave: () -> Unit,
 ) {
-
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -58,141 +69,194 @@ fun NourishmentFormScreen(
             },
             actions = {}
         )
-        Column(
-            modifier = Modifier
-                .padding(10.dp)
+        Box(
+            Modifier.padding(10.dp)
         ) {
             Text(
                 text = "Add Nourishment",
                 style = MaterialTheme.typography.displayMedium
             )
+        }
 
-            OutlinedTextField(
-                value = "",
-                onValueChange = {},
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 16.dp),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Text,
-                    imeAction = ImeAction.Next
-                ),
-                placeholder = {
-                    Text("Name")
-                },
-                shape = MaterialTheme.shapes.medium,
-                colors = TextFieldDefaults.colors(
-                    unfocusedIndicatorColor = Color.Transparent,
-                    focusedIndicatorColor = Color.Transparent,
-                    disabledIndicatorColor = Color.Transparent,
-                    errorIndicatorColor = Color.Transparent
-                ),
-            )
+        LazyColumn(
+            modifier = Modifier
+                .padding(10.dp)
+        ) {
+            item {
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Next
+                    ),
+                    placeholder = {
+                        Text("Name")
+                    },
+                    shape = MaterialTheme.shapes.medium,
+                    colors = TextFieldDefaults.colors(
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        errorIndicatorColor = Color.Transparent
+                    ),
+                )
 
-            OutlinedTextField(
-                value = "",
-                onValueChange = {},
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 16.dp),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Text,
-                    imeAction = ImeAction.Next
-                ),
-                placeholder = {
-                    Text("Description")
-                },
-                shape = MaterialTheme.shapes.medium,
-                colors = TextFieldDefaults.colors(
-                    unfocusedIndicatorColor = Color.Transparent,
-                    focusedIndicatorColor = Color.Transparent,
-                    disabledIndicatorColor = Color.Transparent,
-                    errorIndicatorColor = Color.Transparent
-                ),
-            )
+                val imageUri = remember { mutableStateOf<Uri?>(null) }
 
-            val radioOptions = listOf("Unit", "Percentage")
-            val (selectedOption, setSelectedOption) = remember {
-                mutableStateOf(radioOptions[0])
-            }
-            Column(
-                modifier = Modifier
-                    .selectableGroup()
-                    .padding(vertical = 8.dp)
-            ) {
-                radioOptions.forEach { text ->
-                    Row(
+                val launcher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.GetContent()
+                ) { uri: Uri? ->
+                    imageUri.value = uri
+                }
+
+                Spacer(Modifier.height(16.dp))
+
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    shape = MaterialTheme.shapes.small,
+                    onClick = {
+                        launcher.launch("image/*")
+                    }
+                ) {
+                    Text(
+                        text = "Select Image",
+                    )
+                }
+
+                imageUri.value?.let { uri ->
+                    DisplayImage(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .height(56.dp)
-                            .selectable(
+                            .size(200.dp)
+                            .padding(16.dp)
+                            .clip(RoundedCornerShape(8.dp)),
+                        image = uri,
+                        contentDescription = "Selected Image",
+                    )
+
+                    val imageUrl = uri.toString()
+                    println("Image Url $imageUrl")
+                }
+
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Next
+                    ),
+                    placeholder = {
+                        Text("Description")
+                    },
+                    shape = MaterialTheme.shapes.medium,
+                    colors = TextFieldDefaults.colors(
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        errorIndicatorColor = Color.Transparent
+                    ),
+                )
+
+                val radioOptions = listOf("Unit", "Percentage")
+                val (selectedOption, setSelectedOption) = remember {
+                    mutableStateOf(radioOptions[0])
+                }
+                Column(
+                    modifier = Modifier
+                        .selectableGroup()
+                        .padding(vertical = 8.dp)
+                ) {
+                    radioOptions.forEach { text ->
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(56.dp)
+                                .selectable(
+                                    selected = (text == selectedOption),
+                                    onClick = {
+                                        setSelectedOption(text)
+                                    },
+                                    role = Role.RadioButton
+                                )
+                                .padding(horizontal = 16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            RadioButton(
                                 selected = (text == selectedOption),
                                 onClick = {
                                     setSelectedOption(text)
                                 },
-                                role = Role.RadioButton
                             )
-                            .padding(horizontal = 16.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        RadioButton(
-                            selected = (text == selectedOption),
-                            onClick = {
-                                setSelectedOption(text)
+                            Text(text = text)
+                        }
+                    }
+                }
+
+                when (selectedOption) {
+                    "Unit" -> {
+                        OutlinedTextField(
+                            value = "",
+                            onValueChange = {},
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 16.dp),
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Number
+                            ),
+                            placeholder = {
+                                Text("Unit")
                             },
+                            shape = MaterialTheme.shapes.medium,
+                            colors = TextFieldDefaults.colors(
+                                unfocusedIndicatorColor = Color.Transparent,
+                                focusedIndicatorColor = Color.Transparent,
+                                disabledIndicatorColor = Color.Transparent,
+                                errorIndicatorColor = Color.Transparent
+                            ),
                         )
-                        Text(text = text)
+                    }
+
+                    "Percentage" -> {
+                        OutlinedTextField(
+                            value = "",
+                            onValueChange = {},
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 16.dp),
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Number
+                            ),
+                            placeholder = {
+                                Text("Percentage")
+                            },
+                            shape = MaterialTheme.shapes.medium,
+                            colors = TextFieldDefaults.colors(
+                                unfocusedIndicatorColor = Color.Transparent,
+                                focusedIndicatorColor = Color.Transparent,
+                                disabledIndicatorColor = Color.Transparent,
+                                errorIndicatorColor = Color.Transparent
+                            ),
+                        )
                     }
                 }
             }
 
-            when (selectedOption) {
-                "Unit" -> {
-                    OutlinedTextField(
-                        value = "",
-                        onValueChange = {},
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 16.dp),
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Number
-                        ),
-                        placeholder = {
-                            Text("Unit")
-                        },
-                        shape = MaterialTheme.shapes.medium,
-                        colors = TextFieldDefaults.colors(
-                            unfocusedIndicatorColor = Color.Transparent,
-                            focusedIndicatorColor = Color.Transparent,
-                            disabledIndicatorColor = Color.Transparent,
-                            errorIndicatorColor = Color.Transparent
-                        ),
-                    )
-                }
-
-                "Percentage" -> {
-                    OutlinedTextField(
-                        value = "",
-                        onValueChange = {},
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 16.dp),
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Number
-                        ),
-                        placeholder = {
-                            Text("Percentage")
-                        },
-                        shape = MaterialTheme.shapes.medium,
-                        colors = TextFieldDefaults.colors(
-                            unfocusedIndicatorColor = Color.Transparent,
-                            focusedIndicatorColor = Color.Transparent,
-                            disabledIndicatorColor = Color.Transparent,
-                            errorIndicatorColor = Color.Transparent
-                        ),
-                    )
-                }
-            }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NourishmentFormScreenPreview() {
+    NourishmentFormScreen(
+        navigateUp = {},
+        onSave = {}
+    )
 }
