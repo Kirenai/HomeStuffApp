64d426cc5e06babb3b39eeb0882aad0ccd4fbe27
Kirenai <pool_dota2@hotmail.com>
2025-07-26 15:42:39 -0500

    feat(navigation): implement BottomBar component for navigation

--- app/src/main/java/me/kire/re/homestuffapp/presentation/homestuff_navigator/HomeStuffNavigator.kt
+++ app/src/main/java/me/kire/re/homestuffapp/presentation/homestuff_navigator/HomeStuffNavigator.kt
@@ -5,23 +5,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.List
-import androidx.compose.material.icons.automirrored.outlined.List
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.ShoppingCart
-import androidx.compose.material3.Badge
-import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -32,9 +21,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -53,6 +39,7 @@ import me.kire.re.homestuffapp.presentation.home.HomeScreen
 import me.kire.re.homestuffapp.presentation.home.HomeViewModel
 import me.kire.re.homestuffapp.presentation.home.category.CategoryFormScreen
 import me.kire.re.homestuffapp.presentation.home.category.CategoryFormViewModel
+import me.kire.re.homestuffapp.presentation.homestuff_navigator.components.BottomBar
 import me.kire.re.homestuffapp.presentation.homestuff_navigator.components.TopAppBar
 import me.kire.re.homestuffapp.presentation.navigation.Route
 import me.kire.re.homestuffapp.presentation.nourishment.NourishmentScreen
@@ -76,27 +63,6 @@ data class BottomNavigationItem(
 
 @Composable
 fun HomeStuffNavigator() {
-    val items = listOf(
-        BottomNavigationItem(
-            title = "Home",
-            selectedIcon = Icons.Filled.Home,
-            unselectedIcon = Icons.Outlined.Home,
-            hasNews = false
-        ),
-        BottomNavigationItem(
-            title = "Lists",
-            selectedIcon = Icons.AutoMirrored.Filled.List,
-            unselectedIcon = Icons.AutoMirrored.Outlined.List,
-            hasNews = false
-        ),
-        BottomNavigationItem(
-            title = "Shopping",
-            selectedIcon = Icons.Filled.ShoppingCart,
-            unselectedIcon = Icons.Outlined.ShoppingCart,
-            hasNews = false
-        ),
-    )
-
     var selectedItem by rememberSaveable {
         mutableIntStateOf(0)
     }
@@ -153,70 +119,10 @@ fun HomeStuffNavigator() {
         },
         bottomBar = {
             if (!isBottomBarVisible) return@Scaffold
-            NavigationBar(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .drawBehind {
-                        val strokeWidth = 1.dp.toPx()
-                        val y = 0f
-
-                        drawLine(
-                            color = Color.LightGray,
-                            start = Offset(0f, y),
-                            end = Offset(size.width, y),
-                            strokeWidth = strokeWidth
-                        )
-                    },
-                containerColor = MaterialTheme.colorScheme.background,
-            ) {
-                items.forEachIndexed { index, item ->
-                    NavigationBarItem(
-                        selected = index == selectedItem,
-                        onClick = {
-                            selectedItem = index
-                            when (index) {
-                                0 -> navigateToTab(
-                                    navController = navController,
-                                    route = Route.HomeScreen.route
-                                )
-
-                                1 -> navigateToTab(
-                                    navController = navController,
-                                    route = Route.NourishmentScreen.route
-                                )
-
-                                2 -> navigateToTab(
-                                    navController = navController,
-                                    route = Route.ShoppingScreen.route
-                                )
-                            }
-                        },
-                        label = {
-                            Text(text = item.title)
-                        },
-                        icon = {
-                            BadgedBox(
-                                badge = {
-                                    if (item.badgeCount != null) {
-                                        Badge {
-                                            Text(text = item.badgeCount.toString())
-                                        }
-                                    } else if (item.hasNews) {
-                                        Badge()
-                                    }
-                                }
-                            ) {
-                                Icon(
-                                    imageVector = if (index == selectedItem) {
-                                        item.selectedIcon
-                                    } else item.unselectedIcon,
-                                    contentDescription = item.title
-                                )
-                            }
-                        }
-                    )
-                }
-            }
+            BottomBar(
+                navController = navController,
+                selectedItem = selectedItem
+            )
         },
         floatingActionButton = {
             when (navController.currentDestination?.route) {
@@ -379,7 +285,7 @@ fun HomeStuffNavigator() {
     }
 }
 
-private fun navigateToTab(navController: NavHostController, route: String) {
+fun navigateToTab(navController: NavHostController, route: String) {
     navController.navigate(route) {
         navController.graph.startDestinationRoute?.let { screenRoute ->
             popUpTo(screenRoute) {
