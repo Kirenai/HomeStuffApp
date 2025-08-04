18841a7fa4e68e25a3ccf1874d5782fb7a2481a6
Kirenai <pool_dota2@hotmail.com>
2025-08-04 03:21:40 -0500

    feat(shopping): update quantity handling to use TextField and remove quantity buttons

--- app/src/main/java/me/kire/re/homestuffapp/presentation/shopping/form/ShoppingEditScreen.kt
+++ app/src/main/java/me/kire/re/homestuffapp/presentation/shopping/form/ShoppingEditScreen.kt
@@ -1,43 +1,32 @@
 package me.kire.re.homestuffapp.presentation.shopping.form
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import me.kire.re.homestuffapp.R
 import me.kire.re.homestuffapp.domain.model.Shopping
 import me.kire.re.homestuffapp.presentation.shopping.ShoppingEvent
 
@@ -47,7 +36,7 @@ fun ShoppingEditScreen(
     shopping: Shopping,
     event: (ShoppingEvent) -> Unit
 ) {
-    var quantity by remember { mutableIntStateOf(shopping.quantity?.toInt() ?: 1) }
+    var quantity by remember { mutableStateOf(shopping.quantity ?: "") }
     var storeName by remember { mutableStateOf(shopping.store ?: "") }
     var price by remember { mutableStateOf(shopping.price ?: "") }
 
@@ -72,50 +61,79 @@ fun ShoppingEditScreen(
                         lineHeight = 24.sp,
                     )
                 )
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    IconButton(
-                        onClick = { quantity = maxOf(1, quantity - 1) },
-                        modifier = Modifier
-                            .clip(MaterialTheme.shapes.extraLarge)
-                            .background(MaterialTheme.colorScheme.surfaceBright)
-                            .size(28.dp)
-                    ) {
-                        Icon(
-                            modifier = Modifier
-                                .size(width = 18.dp, height = 24.dp),
-                            painter = painterResource(R.drawable.outline_remove_24),
-                            contentDescription = "Decrease quantity",
-                            tint = MaterialTheme.colorScheme.secondary,
+//                Row(
+//                    modifier = Modifier.fillMaxWidth(),
+//                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+//                    verticalAlignment = Alignment.CenterVertically
+//                ) {
+//                    IconButton(
+//                        onClick = { quantity = maxOf(1, quantity - 1) },
+//                        modifier = Modifier
+//                            .clip(MaterialTheme.shapes.extraLarge)
+//                            .background(MaterialTheme.colorScheme.surfaceBright)
+//                            .size(28.dp)
+//                    ) {
+//                        Icon(
+//                            modifier = Modifier
+//                                .size(width = 18.dp, height = 24.dp),
+//                            painter = painterResource(R.drawable.outline_remove_24),
+//                            contentDescription = "Decrease quantity",
+//                            tint = MaterialTheme.colorScheme.secondary,
+//                        )
+//                    }
+//                    Text(
+//                        text = quantity.toString(), // Replace with actual quantity
+//                        style = MaterialTheme.typography.bodyLarge.copy(
+//                            fontSize = 20.sp,
+//                            lineHeight = 24.sp,
+//                            color = MaterialTheme.colorScheme.onSurface,
+//                        )
+//                    )
+//                    IconButton(
+//                        onClick = { quantity = quantity.plus(1) },
+//                        modifier = Modifier
+//                            .clip(MaterialTheme.shapes.extraLarge)
+//                            .background(MaterialTheme.colorScheme.surfaceBright)
+//                            .size(28.dp)
+//                    ) {
+//                        Icon(
+//                            modifier = Modifier
+//                                .size(width = 19.dp, height = 22.dp),
+//                            imageVector = Icons.Outlined.Add,
+//                            contentDescription = "Increase quantity",
+//                            tint = MaterialTheme.colorScheme.secondary,
+//                        )
+//                    }
+//                }
+
+                TextField(
+                    modifier = Modifier
+                        .height(48.dp)
+                        .fillMaxWidth(),
+                    value = quantity,
+                    onValueChange = {
+                        quantity = it
+                    },
+                    placeholder = {
+                        Text(
+                            text = "15",
+                            style = MaterialTheme.typography.bodySmall,
                         )
-                    }
-                    Text(
-                        text = quantity.toString(), // Replace with actual quantity
-                        style = MaterialTheme.typography.bodyLarge.copy(
-                            fontSize = 20.sp,
-                            lineHeight = 24.sp,
-                            color = MaterialTheme.colorScheme.onSurface,
-                        )
-                    )
-                    IconButton(
-                        onClick = { quantity = quantity.plus(1) },
-                        modifier = Modifier
-                            .clip(MaterialTheme.shapes.extraLarge)
-                            .background(MaterialTheme.colorScheme.surfaceBright)
-                            .size(28.dp)
-                    ) {
-                        Icon(
-                            modifier = Modifier
-                                .size(width = 19.dp, height = 22.dp),
-                            imageVector = Icons.Outlined.Add,
-                            contentDescription = "Increase quantity",
-                            tint = MaterialTheme.colorScheme.secondary,
-                        )
-                    }
-                }
+                    },
+                    maxLines = 1,
+                    shape = MaterialTheme.shapes.medium,
+                    colors = TextFieldDefaults.colors(
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        errorIndicatorColor = Color.Transparent
+                    ),
+                    textStyle = MaterialTheme.typography.bodySmall,
+                    keyboardOptions = KeyboardOptions(
+                        imeAction = ImeAction.Next,
+                        keyboardType = KeyboardType.Number
+                    ),
+                )
             }
         }
         Row(
@@ -227,7 +245,7 @@ fun ShoppingEditScreen(
                     event(
                         ShoppingEvent.EditItem(
                             shopping.copy(
-                                quantity = quantity.toString(),
+                                quantity = quantity,
                                 store = storeName,
                                 price = price
                             )
